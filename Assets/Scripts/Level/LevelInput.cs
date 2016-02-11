116d21d9454ffc38cae37b532f290e71d0a5581a
Meruem <martin.kuben@seznam.cz>
2016-02-11 13:26:42 +0100

    Update LevelInput.cs

--- Assets/Scripts/Level/LevelInput.cs
+++ Assets/Scripts/Level/LevelInput.cs
@@ -2,21 +2,13 @@
 
 namespace Assets.Scripts
 {
-    [RequireComponent(typeof(LevelGeneratorScript))]
     class LevelInput : MonoBehaviour
     {
-        private LevelGeneratorScript _level;
-
-        void Awake()
-        {
-            _level = GetComponent<LevelGeneratorScript>();
-        }
-
         void Update()
         {
             if (Input.GetKeyDown(KeyCode.R))
             {
-                _level.Restart();
+                this.GetPubSub().PublishMessageGlobal(new RestartLevelMessage());
             }
         }
     }
