cd373dff9f3bcd8eb70f9b35f2b306707d20d3c5
Meruem <martin.kuben@seznam.cz>
2016-02-11 14:09:46 +0100

    Create GUIHealthNotify.cs

---
+++ Assets/Scripts/Actors/GUIHealthNotify.cs
@@ -0,0 +1,17 @@
+namespace Assets.Scripts.Actors
+{
+    [ReguireComponent(typeof(Health))]
+    public class GUIHealthNotify : MonoBehaviour
+    {
+        public void Start()
+        {
+            var health = GetComponent<Health>();
+            var originalAction = health.OnTakeDamageAction;
+            health.OnTakeDamageAction = dmg => 
+                {
+                    originalAction(dmg);
+                    PubSub.GlobalPubSub.Publish(new GUIHealthUpdateMessage(health.Lives));
+                }
+        }
+    }
+}
